90bbfdb496ef83895b1edd3e0b974de380178465
Isaaaca <isaaaca@gmail.com>
2020-07-23 17:15:54 +0800

    Created rooms and Contraint Camera to room bounds

---
+++ Assets/Scripts/Room.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Room : MonoBehaviour
+{
+    public static event Action<Room> OnEnterRoom = (room) => { };
+
+    [SerializeField] private float topBorder = 1;
+    private PolygonCollider2D col2D;
+    private void Awake()
+    {
+        Collider2D[] colliders = GetComponentsInChildren<Collider2D>();
+        Bounds bounds = colliders[0].bounds;
+        foreach (Collider2D col in colliders)
+        {
+            bounds.Encapsulate(col.bounds);
+        }
+        bounds.center += Vector3.up * (topBorder / 2);
+        bounds.center -= transform.position;
+        bounds.size += Vector3.up * topBorder;
+        bounds.size -= Vector3.right*0.5f;
+
+        col2D = gameObject.AddComponent<PolygonCollider2D>();
+
+        col2D.isTrigger = true;
+        col2D.pathCount = 1;
+        col2D.points = new Vector2[4]
+            {
+                new Vector2(bounds.center.x + bounds.extents.x, bounds.center.y + bounds.extents.y),
+                new Vector2(bounds.center.x + bounds.extents.x, bounds.center.y - bounds.extents.y),
+                new Vector2(bounds.center.x - bounds.extents.x, bounds.center.y - bounds.extents.y),
+                new Vector2(bounds.center.x - bounds.extents.x, bounds.center.y + bounds.extents.y),
+            };
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        OnEnterRoom(this);
+    }
+
+    public Collider2D GetCollider2D()
+    {
+        return col2D;
+    }
+}
---
+++ Assets/Scripts/RoomCamera.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoomCamera : MonoBehaviour
+{
+    private Cinemachine.CinemachineConfiner confiner;
+
+    private void Awake()
+    {
+        Room.OnEnterRoom += ChangeRoom;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        confiner = GetComponent<Cinemachine.CinemachineConfiner>();
+    }
+
+    void ChangeRoom(Room room)
+    {
+        confiner.InvalidatePathCache();
+        confiner.m_BoundingShape2D = room.GetCollider2D();
+    }
+}
