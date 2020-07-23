f53013200e1f208acecdda48cd9918cfe0dd8836
Isaaaca <isaaaca@gmail.com>
2020-07-23 17:16:55 +0800

    Created Interatable Prefab

--- Assets/Scripts/Interactable.cs
+++ Assets/Scripts/Interactable.cs
@@ -3,13 +3,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Interactable : MonoBehaviour
+public abstract class Interactable : MonoBehaviour
 {
 
-    public static event Action<Dialogue> OnInteractEvent = (dialogue) => { };
-    
-    private bool interactable = false;
-    [SerializeField]private Dialogue message = null;
+    public static event Action<string, Interactable> OnInteractEvent = (id, interactable) => { };
+
+    [SerializeField] private SpriteRenderer tooltip = null;
+
+    private bool focused = false;
+    private bool interactable = true;
+    protected string id;
+
+    private void Awake()
+    {
+        DialogueManager.OnDialogueStartEnd += (dialogueActive) => SetInteractable(!dialogueActive);
+    }
+
 
     // Start is called before the first frame update
     void Start()
@@ -20,17 +29,59 @@ public class Interactable : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (interactable)
+        if (focused && interactable)
         {
-            if (Input.GetKeyDown(KeyCode.R)) OnInteractEvent(message);
+            if (Input.GetKeyDown(KeyCode.R)) OnInteract();
         }
     }
 
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected virtual void OnInteract()
+    {
+        OnInteractEvent(id, this);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
     {
         if (collision.name.Equals("Player"))
         {
-            interactable = true;   
+            if (collision.gameObject.GetComponent<PlayerController>().IsFacing(this.transform.position))
+            {
+                Focus();
+
+            }
+            else
+            {
+                Unfocus();
+
+            }
         }
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        Unfocus();
+    }
+
+    private void Focus()
+    {
+        if (!focused)
+        {
+            focused = true;
+            tooltip.enabled = true;
+        }
+    }
+
+    private void Unfocus()
+    {
+        if (focused)
+        {
+            focused = false;
+            tooltip.enabled = false;
+        }
+    }
+
+
+    public void SetInteractable(bool interactable)
+    {
+        this.interactable = interactable;
     }
 }
