cd313b2780caec2c47649bd2c05493c94857078b
Isaaaca <isaaaca@gmail.com>
2020-07-23 17:20:21 +0800

    Added gameManager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] DialogueManager dialogueManager = null;
+
+    private void Awake()
+    {
+        Interactable.OnInteractEvent += HandleInteractEvent;
+        Room.OnEnterRoom += HandleEnterRoomEvent;
+    }
+
+    private void HandleEnterRoomEvent(Room room)
+    {
+        print(room.name);
+    }
+
+    private void HandleInteractEvent(string id, Interactable interactable)
+    {
+        string type = id.Substring(0,3);
+        switch (type)
+        {
+            case "OBJ":
+                ObservableObject obj = interactable as ObservableObject;
+                dialogueManager.LoadDialogue(obj.message);
+                break; 
+        }
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
