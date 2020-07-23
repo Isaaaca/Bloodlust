93262486816998c73c45dc2835efde7004b4f113
Isaaaca <isaaaca@gmail.com>
2020-07-23 17:18:10 +0800

    Created Observable Object (inherits from interactable)

---
+++ Assets/Scripts/ObservableObject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObservableObject : Interactable
+{
+
+
+    public Dialogue message = null;
+    // Start is called before the first frame update
+    void Start()
+    {
+        id = gameObject.name;
+    }
+
+}
