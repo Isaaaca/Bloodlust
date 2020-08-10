2aae7324e9b9988e3dc2ab7e38578b39663d7c7f
Isaaaca <isaaaca@gmail.com>
2020-08-10 18:21:41 +0800

    Started on Saving system

---
+++ Assets/Scripts/SaveManager.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SaveManager 
+{
+    private static HashSet<string> gameProgress = new HashSet<string>();
+
+    public static void AddEvent(string eventCode)
+    {
+        gameProgress.Add(eventCode);
+    }
+
+    public static bool GetEvent(string eventCode)
+    {
+        return gameProgress.Contains(eventCode);
+    }
+   
+}
