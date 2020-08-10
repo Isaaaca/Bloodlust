08a1e4ef45c12e27493beda22c5b90ac437e4828
Isaaaca <isaaaca@gmail.com>
2020-08-10 18:52:09 +0800

    Started on NPCs

---
+++ Assets/Scripts/Interactables/NPC.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : Interactable
+{
+    public enum ConversationType
+    {
+        Cycle,
+        Onetime
+    }
+
+    [SerializeField]private ConversationType convoType;
+    [SerializeField]private Dialogue[] dialogues;
+    [SerializeField]private int currDialogue = 0;
+
+    public Dialogue GetDialogue()
+    {
+        return dialogues[currDialogue];
+    }
+
+    protected override void OnInteract()
+    {
+        base.OnInteract();
+        if (convoType == ConversationType.Cycle)
+        {
+            currDialogue = (currDialogue + 1) % dialogues.Length;
+        }
+        else if (convoType == ConversationType.Onetime)
+        {
+            currDialogue += currDialogue < (dialogues.Length - 1) ? 1 : 0;
+        }
+    }
+}
